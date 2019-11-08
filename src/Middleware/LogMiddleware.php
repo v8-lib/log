3bc943ec341b7040f7b92617d5ff38e7dc81be27
Ren <renlongfei@qianqiantech.com>
2019-11-08 11:02:08 +0800

    chore:修复traceid不显示

--- src/Middleware/LogMiddleware.php
+++ src/Middleware/LogMiddleware.php
@@ -5,51 +5,53 @@
  * Date: 2019/7/11
  * Time: 8:40 PM
  */
-declare(strict_types=1);
+declare (strict_types=1);
 
 namespace V8\Log\Middleware;
 
-use Psr\Container\ContainerInterface;
-use Psr\Http\Server\MiddlewareInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
-use Psr\Http\Message\ResponseInterface;
 use Hyperf\Utils\Context;
-use Hyperf\Utils\Coroutine;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 class LogMiddleware implements MiddlewareInterface
 {
-	/**
-	 * @var ContainerInterface
-	 */
-	protected $container;
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
 
-	public function __construct(ContainerInterface $container)
-	{
-		$this->container = $container;
-	}
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * @param ServerRequestInterface $request
-	 * @param RequestHandlerInterface $handler
-	 * @return ResponseInterface
-	 */
-	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-	{
-		$this->getTraceId();
+    /**
+     * @param ServerRequestInterface  $request
+     * @param RequestHandlerInterface $handler
+     *
+     * @return ResponseInterface
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->getTraceId();
 
-		return $handler->handle($request);
-	}
+        return $handler->handle($request);
+    }
 
-	/**
-	 * 获取traceId
-	 *
-	 * @return mixed|null
-	 */
-	private function getTraceId()
-	{
-		$root    = Context::get('tracer.root');
-		$traceId = $root->getContext()->getTraceId();
-		Context::set('tarce.id', $traceId);
-	}
+    /**
+     * 获取traceId
+     *
+     * @return mixed|null
+     */
+    private function getTraceId()
+    {
+        $root    = Context::get('tracer.root');
+        $traceId = $root->getContext()
+                        ->getContext()
+                        ->getTraceId();
+        Context::set('tarce.id', $traceId);
+    }
 }
