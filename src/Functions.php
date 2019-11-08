55fe1ba4cf641ebb899d4a45aba46ceceea72033
Ren <renlongfei@qianqiantech.com>
2019-11-08 10:13:59 +0800

    chore:添加RMB转换函数

--- src/Functions.php
+++ src/Functions.php
@@ -5,17 +5,17 @@
  * Date: 2019/7/12
  * Time: 3:45 PM
  */
-declare(strict_types=1);
-use Hyperf\Utils\ApplicationContext;
+declare (strict_types = 1);
 use Hyperf\ExceptionHandler\Formatter\FormatterInterface;
+use Hyperf\Utils\ApplicationContext;
+
 if (false === function_exists('di')) {
 	/**
 	 * Finds an entry of the container by its identifier and returns it.
 	 * @param null|mixed $id
 	 * @return mixed|\Psr\Container\ContainerInterface
 	 */
-	function di($id = null)
-	{
+	function di($id = null) {
 		$container = ApplicationContext::getContainer();
 		if ($id) {
 			return $container->get($id);
@@ -30,8 +30,33 @@ if (false === function_exists('format_throwable')) {
 	 * @param Throwable $throwable
 	 * @return string
 	 */
-	function format_throwable(Throwable $throwable): string
-	{
+	function format_throwable(Throwable $throwable): string {
 		return di()->get(FormatterInterface::class)->format($throwable);
+	}
+}
+
+if (false === function_exists('fen2yuan')) {
+	/**
+	 * 人民币分转为人民币元
+	 *
+	 * @param integer $fen
+	 *
+	 * @return float
+	 */
+	function fen2yuan(int $fen) {
+		return number_format($fen / 100, 2, '.', '');
+	}
+}
+
+if (false === function_exists('yuan2fen')) {
+	/**
+	 * 人民币元转为人民币分
+	 *
+	 * @param float $yuan
+	 *
+	 * @return integer
+	 */
+	function yuan2fen($yuan): int {
+		return (int) bcmul($yuan, 100);
 	}
 }
